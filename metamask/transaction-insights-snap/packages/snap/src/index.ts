a8b13f9b537043bbf27f81d2970571c5a94ef7cd
Steve Li <lithafnium2000@gmail.com>
2023-09-23 18:25:47 -0400

    added simulation fetch + parlalelism

--- metamask/transaction-insights-snap/packages/snap/src/index.ts
+++ metamask/transaction-insights-snap/packages/snap/src/index.ts
@@ -5,20 +5,28 @@ const url =
   'https://6yum6wkzha.execute-api.us-east-1.amazonaws.com/test/malicious-contract-classifier';
 
 const opcodes_url = 'https://opcodes.crtsn.com/opcodes?address=';
-const classifyContracts = async (eth_address: string) => {
-  let opcodes = await fetch(`${opcodes_url}${eth_address}`, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    // body data type must match "Content-Type" header
+const simulate_url = 'https://opcodes.crtsn.com/simulate?fromAddr=';
+
+const getOpcodes = async (toAddr: string) => {
+  let opcodes = await fetch(`${opcodes_url}${toAddr}`, {
+    method: 'POST',
   });
-  // @ts-ignore
-  opcodes = await opcodes.json();
-  console.log(opcodes);
+  return await opcodes.json();
+};
 
-  const data = {
-    contract_opcode: opcodes,
-  };
+const simulateTransaction = async (fromAddr: string, value: string) => {
+  let simulate = await fetch(
+    `${simulate_url}${fromAddr}&toAddr=${'0x7Fd1A8EF5875D907AEafF602607c4CC7d1f26C83'}&value=${value}&data=${'0xa9059cbb000000000000000000000000fc43f5f9dd45258b3aff31bdbe6561d97e8b71de00000000000000000000000000000000000000000000000000000000000f4240'}`,
+    {
+      method: 'POST',
+    },
+  );
 
-  const response = await fetch(url, {
+  return await simulate.json();
+};
+
+const getClassification = async (data: any) => {
+  const classification = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -31,18 +39,71 @@ const classifyContracts = async (eth_address: string) => {
     referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
-
-  return await response.json();
+  let json = await classification.json();
+  json['message'] = '';
+  return json;
 };
+
+const classifyContracts = async (
+  fromAddr: string,
+  toAddr: string,
+  value: string,
+) => {
+  let classification = getOpcodes(toAddr).then((opcodes) => {
+    let data;
+    if (opcodes) {
+      data = {
+        contract_opcode: opcodes,
+      };
+    } else {
+      return {
+        message: 'This address is likely **another wallet.**',
+        body: '[{"label": "Benign", "score": 1}]',
+      };
+    }
+
+    return getClassification(data);
+  });
+
+  let simulate = simulateTransaction(fromAddr, value);
+
+  const results = await Promise.allSettled([simulate, classification]);
+  console.log(results[0]);
+  console.log(results[1]);
+  // console.log(classification);
+  // // @ts-ignore
+
+  // simulate = await simulate.json();
+  // console.log(simulate);
+
+  // @ts-ignore
+  return [results[0]['value'], results[1]['value']];
+};
+
+interface Transaction {
+  data?: string;
+  gas?: string;
+  gasPrice?: string;
+  maxPriorityFeePerGas?: string;
+  maxFeePerGas?: string;
+  from: string;
+  to: string;
+  value: string;
+}
 
 // Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
-  let target = transaction['to'];
-  console.log(target);
   // @ts-ignore
-  let classification = await classifyContracts(target);
+  let t: Transaction = transaction;
+  console.log(transaction);
+  let from = t['from'];
+  let to = t['to'];
+  let value = t['value'];
+  let [simulation, classification] = await classifyContracts(from, to, value);
+
+  console.log(simulation);
   console.log(classification);
-  console.log(JSON.stringify(classification));
+
   let body = JSON.parse(classification['body'])[0];
   let label = body['label'];
   let score = body['score'];
@@ -56,14 +117,53 @@ export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
       ? 'Malicious Contract Warning ❌'
       : 'Benign Contract ✅';
 
+  let simulationChildren;
+  if (simulation['error']) {
+    console.log(simulation['error']);
+    simulationChildren = [
+      text(
+        `**This transaction was unable to be simulated**: ${simulation['error']['message']}`,
+      ),
+    ];
+  } else {
+    let simulationChanges = simulation['result']['changes'];
+
+    simulationChildren = simulationChanges.map((c: any) => {
+      return panel([
+        text(
+          `${
+            from == c['from'] ? 'Your Address' : c['from'].substring(0, 10)
+          }... ⏩ ${c['to'].substring(0, 10)}...`,
+        ),
+        text(`**Amount**: ${c['amount']} ${c['symbol']} 💶 `),
+        divider(),
+      ]);
+    });
+
+    simulationChildren.push(
+      panel([
+        text(
+          'Above is a simulation of asset changes after making the above transaction. If this does not look malicious, feel free to ignore this warning.',
+        ),
+      ]),
+    );
+  }
+
+  let simulateChildren =
+    label == 'Malicious'
+      ? [divider(), heading('Simulation Results'), panel(simulationChildren)]
+      : [];
+
   return {
     content: panel([
       heading(`${message}`),
       text(`This smart contract is **${label}** with score _${score}_ `),
+      text(`${classification['message']}`),
       divider(),
       text(
-        'This result was obtained by analyzing opcode information with a transformer based model, trained millions of smart contracts. _This result is just a suggestion, proceed at your own risk._',
+        'This result was obtained by analyzing opcode information with a transformer based model, trained on millions of smart contracts. _This result is just a suggestion, proceed at your own risk._',
       ),
+      panel(simulateChildren),
     ]),
   };
 };
